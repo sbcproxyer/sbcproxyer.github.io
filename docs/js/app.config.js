68f5857ccea78ba09d1d9cb9cc23337135dcf8e7
alen-x <66928150+alen-x@users.noreply.github.com>
2021-10-18 11:36:47 +0800

    Update app.config.js
    update  apkurl

--- docs/js/app.config.js
+++ docs/js/app.config.js
@@ -1,1 +1,1 @@
-window.njconfig={apkurl:"https://dl.testfairy.com/download/74VK2D1G6WT2TC1DHRBCFBWCG24S7R9JX4G7ME44KVBGRG0F/ninja_2.3.0-testfairy.apk",isourl:"",qrdltext:"https://sbcproxyer.github.io/dl/"};
+window.njconfig={apkurl:"https://dl.testfairy.com/download/74VK4C1J6MV2TC1D2GJT0KEZ6BDTMWGC1T0SJTSDBYX080G7/ninja_2.5.0-testfairy.apk",isourl:"",qrdltext:"https://sbcproxyer.github.io/dl/"};
