630f18c695909f66d73ef0d8e9905e6fde730fc0
sbcproxyer <sbcproxyer@gmail.com>
2021-11-26 15:25:04 +0800

    v3.3.0-bfb2cb

--- docs/js/app.config.js
+++ docs/js/app.config.js
@@ -1,1 +1,1 @@
-window.njconfig={apkurl:"https://dl.testfairy.com/download/74VK6CSP64R2TC1DQF94QJ9NTHYFB3C6DQNHYG45HKNBXYA7/ninja_2.8.0-testfairy.apk",isourl:"https://testflight.apple.com/join/2EccEH6p",qrdltext:"https://sbcproxyer.github.io/dl/"};
+window.njconfig={apkurl:"https://dl.testfairy.com/download/74VK4E1M6RRJTC1DZ4GEJNR1BYXNNVM6DHX57XRNMX93HXZ2/ninja_2.6.0-testfairy.apk",isourl:"https://testflight.apple.com/join/2EccEH6p",qrdltext:"https://sbcproxyer.github.io/dl/"};
