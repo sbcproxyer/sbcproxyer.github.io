5c360bc9f21749aedabcf5f6fb4bad961db4ebc0
alen-x <66928150+alen-x@users.noreply.github.com>
2022-01-28 13:49:28 +0800

    Update app.config.js
    update android uri

--- docs/js/app.config.js
+++ docs/js/app.config.js
@@ -1,1 +1,1 @@
-window.njconfig={apkurl:"https://dl.testfairy.com/download/74VKGD9Q64S2TC1DDENQ7GBA86ZZ6PPEY5K8W20ZTQPZ94FV/ninja_3.0.0-testfairy.apk",isourl:"https://testflight.apple.com/join/2EccEH6p",qrdltext:"https://sbcproxyer.github.io/dl/"};
+window.njconfig={apkurl:"https://dl.testfairy.com/download/74W34E1G68V2TC1DDTDR95R34Y2AQPVZKMVG5SGA3PYJTJD8/ninja_3.1.4-testfairy.apk",isourl:"https://testflight.apple.com/join/2EccEH6p",qrdltext:"https://sbcproxyer.github.io/dl/"};
